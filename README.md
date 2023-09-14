26bfa7499e1b1613d59e38194d2a746ff5bf72dd
f00lg0ldl0af <43835575+f00lg0ldl0af@users.noreply.github.com>
2023-09-14 17:56:47 +0800

    Feat - decimal point button

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Calculator
+# Overview
+This is a project under [The Odin Project](https://www.theodinproject.com/about).
+
+# Description 
+This is a on-screen calculator. User can add, subtract, multiply and divide values. 
+
+### Usage
+[View demo]():point_left:
+
+<!-- include demo link  -->
+
+# Inspiration 
+* [Michal Osman](https://michalosman.github.io/calculator/)
--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 const numberBtns = document.querySelectorAll('[number-input]');
 const operatorBtns = document.querySelectorAll('.btn-operator');
-/* Attribute selectors for square brackets, see (https://dev.to/giandodev/selecting-an-element-1nd0)*/
-
-const decimalBtn = document.querySelector('#decimalBtn');
 const equalBtn = document.querySelector('#equalBtn');
-
 const clearAllBtn = document.querySelector('#clearAllBtn');
 const clearEntryBtn = document.querySelector('#clearEntryBtn');
+const decimalBtn = document.querySelector('#decimalBtn');
 
 const prevOperationDisplay = document.querySelector('#prevOperationDisplay');
 const nowOperationDisplay = document.querySelector('#nowOperationDisplay');
@@ -14,17 +11,44 @@ const nowOperationDisplay = document.querySelector('#nowOperationDisplay');
 
 equalBtn.addEventListener('click', evaluate);
 clearAllBtn.addEventListener('click', clearAll);
+function clearAll() {
+    nowOperationDisplay.textContent = '0';
+    prevOperationDisplay.textContent = '';
+    currentOperation = null;
+}
+
 clearEntryBtn.addEventListener('click',clearEntry);
+function clearEntry() {
+    nowOperationDisplay.textContent = nowOperationDisplay.textContent.toString().slice(0,-1);
+}
+decimalBtn.addEventListener('click', addPoint);
+function addPoint() {
+    // After operator is keyed into calculator, keying decimal pt should display "0."
+    if (toClearScreen)
+    {
+        resetScreen();
+    }
 
+    if (nowOperationDisplay === '')
+    {
+        nowOperationDisplay.textContent == '0';
+    }
+    // .. is not valid entry 
+    if (nowOperationDisplay.textContent.includes('.'))
+    {
+        return;
+    }
+    nowOperationDisplay.textContent += '.';
+}
 
-// 5. Create functions that populate the display when I click on number buttons. Store display value in a variable. 
+// Populate calculate display with user input 
 numberBtns.forEach((button) => button.addEventListener('click',() => appendNum(button.textContent)));
 
 function appendNum(num) {
-    if(nowOperationDisplay.textContent === '0' || toClearScreen) // Think when else user needs to clear the screen (i) after keying in firstNum and operator, to enter secondNum
-    
-    // Function to clear screen so user can input new entries
+    if(nowOperationDisplay.textContent === '0' || toClearScreen) 
+    {
         resetScreen();
+    }
     nowOperationDisplay.textContent += num;
 }
 
@@ -33,46 +57,51 @@ function resetScreen() {
     toClearScreen = false;
 }
 
-function clearAll() {
-    nowOperationDisplay.textContent = '0';
-    prevOperationDisplay.textContent = '';
-    currentOperation = null;
-}
-
-function clearEntry() {
-    nowOperationDisplay.textContent = nowOperationDisplay.textContent.toString().slice(0,-1);
-}
-
 operatorBtns.forEach((button) => button.addEventListener('click', () => appendOperator(button.textContent)));
 
-// 6. Store the number inputs when user presses an operator.
 function appendOperator(operator) {
-    // nowOperationDisplay.textContent += operator;
-    if(currentOperation !== null) evaluate();
+    if(currentOperation !== null) 
+    {
+        evaluate(); 
+    }
     firstNum =  nowOperationDisplay.textContent;
     currentOperation = operator;
     prevOperationDisplay.textContent = `${firstNum} ${currentOperation}`;
-    toClearScreen = true; // 
+    toClearScreen = true;
 }
 
-// 6. Save which operation has been chosen and operate() on them when '=' is selected.
+// Calculator can only evaluate with at least 1 operator
 function evaluate() {
     if (currentOperation === null || toClearScreen) return;
-    if (currentOperation === '÷' && nowOperationDisplay.textContent === '0') {
+    if (currentOperation === '÷' && nowOperationDisplay.textContent === '0') 
+    {
         alert("Unable to divide by 0");
         return;
     }
-
     secondNum = nowOperationDisplay.textContent;
     nowOperationDisplay.textContent = operate(currentOperation,firstNum,secondNum).toFixed(2);
     prevOperationDisplay.textContent = `${firstNum} ${currentOperation} ${secondNum} = ` ;
-
     currentOperation = null;
-
 }
 
+function operate(operator,a,b) {
+    a = Number(a);
+    b = Number(b);
+    switch(operator) {
+        case '+':   
+            return add (a,b);    
+        case '-':         
+            return subtract (a,b);    
+        case 'x':   
+            return multiply (a,b);    
+        case '÷':
+            if (b === 0) return null;               
+            return divide(a,b);
+        default:
+            return null;    
+    }
+}
 
-// 1. Functions for basic math operators
 function add(a,b) {
     return a + b;
 }
@@ -87,27 +116,4 @@ function multiply(a,b) {
 
 function divide(a,b) {
     return a / b;
-}
-
-// 2. Create Variables for first number, operator and second number
-let a;
-let b;
-let operator;
-
-// 3. Create Function operate that calls above functions on the numbers
-function operate(operator,a,b) {
-    switch(operator) {
-        case '+':   
-            return add(a,b);    
-        case '-':         
-            return subtract(a,b);    
-        case 'x':   
-            return multiply(a,b);    
-        case '÷':
-            if (b === 0) return null;               
-            return divide(a,b);
-        default:
-            return null;    
-    }
-
 }
